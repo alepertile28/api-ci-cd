fca9f318469d7a92698c221de52a24a871e9daa2
alepertile28 <a.augustopertile@gmail.com>
2025-07-27 23:52:20 -0300

    flake8 required adjusts

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
 
 tasks = [
     {"id": 1, "task": "Learn Python"},
     {"id": 2, "task": "Write CI/CD pipelines"},
 ]
+
 
 @app.get("/tasks")
 async def get_tasks():
