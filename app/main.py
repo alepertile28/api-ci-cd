55bce968d8457e53b11fada524957ba920fe0798
alepertile28 <a.augustopertile@gmail.com>
2025-07-27 23:28:11 -0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+tasks = [
+    {"id": 1, "task": "Learn Python"},
+    {"id": 2, "task": "Write CI/CD pipelines"},
+]
+
+@app.get("/tasks")
+async def get_tasks():
+    return tasks
