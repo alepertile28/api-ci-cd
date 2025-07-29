bdd24fc0f9e89682927beab2fc09844f2ac06d72
alepertile28 <a.augustopertile@gmail.com>
2025-07-29 10:57:17 -0300

    ajustes no codigo

--- app/main.py
+++ app/main.py
@@ -1,23 +1,26 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
-from statistics import mean, median, mode, StatisticsError
+from statistics import mean, median
 from typing import List
-
+from collections import Counter
 
 app = FastAPI()
-
 
 class Numbers(BaseModel):
     numbers: list[float] = Field(..., example=[1, 2, 3])
 
 @app.post("/stats")
 def get_stats(numbers: Numbers):
-    try:
-        values = numbers.numbers
-        return {
-            "mean": mean(values),
-            "median": median(values),
-            "mode": mode(values)
-        }
-    except StatisticsError:
+    values = numbers.numbers
+    freqs = Counter(values)
+    max_freq = max(freqs.values())
+    modes = [num for num, count in freqs.items() if count == max_freq]
+
+    if len(modes) != 1:
         raise HTTPException(status_code=400, detail="No unique mode found")
+
+    return {
+        "mean": mean(values),
+        "median": median(values),
+        "mode": modes[0]
+    }
